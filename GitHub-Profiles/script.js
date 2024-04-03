9c3869c18192ee6912b742c64adb8ea6644f32bb
baldzhiyski__ <hristo_baldjiiski04@abv.bg>
2024-04-03 12:40:33 +0300

    GitHub Profiles Project Using API

---
+++ GitHub-Profiles/script.js
@@ -0,0 +1,92 @@
+const API_URL = 'https://api.github.com/users/';
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+
+async function getUser(username) {
+    try {
+        const { data } = await axios(API_URL + username)
+
+        createUserCard(data);
+        getRepos(username);
+    } catch(err) {
+        if(err.response.status == 404) {
+            createErrorCard('No such user profile found in GitHub !')
+        }
+    }
+}
+
+async function getRepos(username) {
+    try {
+        const { data } = await axios(API_URL + username + '/repos?sort=created')
+
+        addReposToCard(data);
+    } catch(err) {
+        createErrorCard('Problem with the fetching of repos !')
+    }
+}
+
+function createUserCard(user) {
+    const userID = user.name || user.login;
+    const userBio = user.bio ? `<p>${user.bio}</p>` : '';
+    const cardHTML = `
+    <div class="card">
+    <div>
+      <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+    </div>
+    <div class="user-info">
+      <h2>${userID}</h2>
+      ${userBio}
+      <ul>
+        <li>${user.followers}<strong>Followers</strong></li>
+        <li>${user.following}<strong>Following</strong></li>
+        <li>${user.public_repos}<strong>Repos</strong></li>
+      </ul>
+
+      <div id="repos"></div>
+    </div>
+  </div>
+    `;
+    main.innerHTML = cardHTML;
+    
+}
+
+function createErrorCard(message){
+    const cardHTML = `
+      <div class= "card">
+        <h1>${message}</h1>
+      </div>
+    `;
+
+    main.innerHTML=cardHTML;
+}
+
+function addReposToCard(repos){
+    const reposEl = document.getElementById('repos');
+
+    repos
+    .slice(0,10)
+    .forEach( repo =>{
+        const repoEl = document.createElement('a');
+        repoEl.classList.add('repo');
+        repoEl.href= repo.html_url;
+        repoEl.target = '_blank';
+        repoEl.innerText = repo.name;
+
+        reposEl.appendChild(repoEl);
+    })
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const user = search.value
+
+    if(user) {
+        getUser(user)
+
+        search.value = ''
+    }
+});
